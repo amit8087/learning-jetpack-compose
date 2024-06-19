6137a42af1de883677ef46f7eff1f5cc54e00145
Amit Gaikwad <amit84088@gmail.com>
2024-06-19 15:48:05 +0530

    understanding the remember and rememberSaveable

--- app/src/main/java/com/example/learningjetpackcompose/SecondScreen.kt
+++ app/src/main/java/com/example/learningjetpackcompose/SecondScreen.kt
@@ -3,18 +3,50 @@ package com.example.learningjetpackcompose
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SecondScreen() {
+    // remember -> Will not save the value after Configuration changes
+    // rememberSaveable -> Will save the value after Configuration changes
+    var name by rememberSaveable {
+        mutableStateOf("")
+    }
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Second Screen")
+        MyText(name)
+        MyTextField(name, onNameChange = {
+            name = it
+        })
     }
+}
+
+@Composable
+fun MyTextField(name: String, onNameChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = name,
+        onValueChange = {
+            onNameChange(it)
+        },
+        label = {
+            Text(text = "Enter your name")
+        })
+}
+
+@Composable
+fun MyText(name: String) {
+    Text(text = "Hello $name", style = TextStyle(fontSize = 30.sp))
 }
